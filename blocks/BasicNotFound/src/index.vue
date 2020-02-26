d9f59cc6db2c60c3a1580b4ee24bc5411c95b1cb
cui <664593049@qq.com>
2020-02-26 11:01:35 +0800

    chore: 新增demo

---
+++ blocks/BasicNotFound/src/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="basic-not-found">
+        <div class={exceptionContent}>
+            <img :src="require('./images/TB1txw7bNrI8KJjy0FpXXb5hVXa-260-260.png')"
+                 class="imgException"
+                 alt="页面不存在"
+            />
+            <div class="prompt">
+                <h3 class="title">
+                    抱歉，你访问的页面不存在
+                </h3>
+                <p class="description">
+                    您要找的页面没有找到，请返回<a href="#">首页</a>继续浏览
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'BasicNotFound'
+}
+</script>
+<style>
+    @import "index.scss";
+</style>
---
+++ blocks/EntryCard/src/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div v-for="(item, index) in list" :key="index" class="item">
+            <a :href="item.url" class="link" target="_blank" rel="noopener noreferrer">
+                <img :src="item.img" class="cover" :alt="item.title" />
+                <div class="title">{item.title}</div>
+            </a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'EntryCard',
+    data () {
+        return {
+            list: [
+                {
+                    title: '帖子',
+                    img: '//gw.alicdn.com/tfscom/TB1OyT.RVXXXXcpXXXXXXXXXXXX.png',
+                    url: '//www.taobao.com',
+                },
+                {
+                    title: '宝贝清单',
+                    img: '//img.alicdn.com/tfs/TB1g6cGRFXXXXa9XXXXXXXXXXXX-140-140.png',
+                    url: '//www.taobao.com',
+                },
+                {
+                    title: '图片',
+                    img: '//img.alicdn.com/tfs/TB1hJ7dRFXXXXcgXFXXXXXXXXXX-140-140.png',
+                    url: '//www.taobao.com',
+                },
+                {
+                    title: '上新',
+                    img: '//img.alicdn.com/tfs/TB196v1RFXXXXb6aXXXXXXXXXXX-140-140.png',
+                    url: '//www.taobao.com',
+                },
+                {
+                    title: '短视频',
+                    img: '//gw.alicdn.com/tfscom/TB1toY.RVXXXXcuXXXXXXXXXXXX.png',
+                    url: '//www.taobao.com',
+                },
+                {
+                    title: '短视频',
+                    img: '//gw.alicdn.com/tfscom/TB1toY.RVXXXXcuXXXXXXXXXXXX.png',
+                    url: '//www.taobao.com',
+                },
+            ]
+        }
+    }
+}
+</script>
+<style>
+    @import "index.scss";
+</style>
